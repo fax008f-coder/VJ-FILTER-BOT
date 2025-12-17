4a6687e6dd417d70131cacd3e52f059b22f4e350
fax008f-coder <fax008f@gmail.com>
2025-12-17 05:40:45 +0530

    Update info.py

--- info.py
+++ info.py
@@ -17,7 +17,7 @@ BOT_TOKEN = environ.get('BOT_TOKEN', "")
 
 
 # This Pictures Is For Start Message Picture, You Can Add Multiple By Giving One Space Between Each.
-PICS = (environ.get('PICS', 'https://i.ibb.co/TBfWBFyK/IMG-20250616-WA0014.jpg')).split()
+PICS = (environ.get('PICS', 'https://files.catbox.moe/81hufs.jpg https://files.catbox.moe/9kbgbh.jpg https://files.catbox.moe/g7uft3.jpg https://files.catbox.moe/6k870i.jpg')).split()
 
 
 # Admins & Users
@@ -196,6 +196,7 @@ else:
 # Don't Remove Credit @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot @Tech_VJ
 # Ask Doubt on telegram @KingVJ01
+
 
 
 
